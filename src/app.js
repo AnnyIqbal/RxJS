a47888e6a035d0358e2ea17593bbde164a53ed97
AnnyIqbal <quratulann_iqbal@hotmail.com>
2016-12-29 16:30:57 +0500

    promises as observables

--- src/app.js
+++ src/app.js
@@ -30,5 +30,45 @@ a$
 });
 */
 
-// OBSERVABLES FROM PROMISE
+// OBSERVABLES FROM A PROMISE, i.e. a promise represents the eventual results of an async operation
 
+const myPromise = new Promise((resolve, reject) => {
+    console.log("Creating Promise");
+    setTimeout(()=>{
+        resolve('Hello world');
+    }, 2000);
+});
+/*
+myPromise.then(x => {
+    console.log(x);
+});
+*/
+// use this promise as an Observable
+/*
+const promise$ = Rx.Observable.fromPromise(myPromise);
+promise$.subscribe(
+    x => {
+        console.log(x);
+    }
+);
+*/
+
+function getUser(username) {
+    return $.ajax({
+        url: 'https://api.github.com/users/' + username,
+        dataType: 'jsonp'
+    }).promise();
+}
+
+
+const input$ = Rx.Observable.fromEvent($('#input'), 'keyup');
+input$.subscribe( e => {
+    Rx.Observable.fromPromise(getUser(e.target.value))
+    .subscribe(x=> {
+        console.log(x);
+        $('#name').text(x.data.name);
+        $('#url').text(x.data.url);
+        $('#repos').text("Public Repos: " + x.data.public_repos);
+    });
+});
+    //46:.. yhn error ni hai mgr thek kam b ni krha chk it kal
