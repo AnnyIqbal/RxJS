fa5b31f5766ef51d5a716243e1f3c0ea7dabd873
AnnyIqbal <quratulann_iqbal@hotmail.com>
2016-12-31 15:40:26 +0500

    not getting step9 and 10

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
+// import { Observable } from 'rxjs/';
+// import Rx from 'rxjs/Rx';
 "use strict";
-var _1 = require('rxjs/');
+//step 02: filter
 // const source = Observable.fromEvent(document, 'click')
 //     .filter( function(x: MouseEvent) {
 //         return x.clientX > window.innerWidth / 2; // returns the clientX values greater than half of window's innerwidth
@@ -9,16 +11,60 @@ var _1 = require('rxjs/');
 //         function(x: MouseEvent){
 //             console.log('x: ' + x.clientX + ', y: ' + x.clientY);
 //     });
-var source = _1.Observable.create(function (x) {
-    x.next('Abc');
-    x.next('Xyz');
-    x.next('Pqr');
-    x.complete();
-});
-source.subscribe(function (x) {
-    console.log('Next : ' + x);
-}, function (err) {
-    console.log(err);
-}, function (complete) {
-    console.log('Completed');
-});
+//step 03: create
+// const source : Rx.Observable<string> = Observable.create(
+//     function(x: Rx.Observer<string>) {
+//         x.next('Abc');
+//         x.next('Xyz');
+//         x.next('Pqr');
+//         x.complete();
+// });
+// source.subscribe(
+//     x => {
+//         console.log('Next : ' +x);
+//     },
+//     err => {
+//         console.log(err);
+//     },
+//     function() {
+//         console.log('Completed');
+//     });
+//step 04 : ajax ni smjh aya??
+//step06: merge
+// import Rx = require('rxjs/Rx');
+// var a = Rx.Observable.interval(200).map(function(i: number) {
+//     return 'A' + i;
+// });
+// var b = Rx.Observable.interval(100).map(function(i: number) {
+//     return 'B' + i;
+// });
+// var m = Rx.Observable.merge(a, b)
+//     .take(10)
+//     .subscribe(x => { console.log(x);});
+//step08: filter even: filters an observable according to condition
+// import Rx = require('rxjs/Rx');
+// var src = Rx.Observable.range(1,5)
+//     .filter(function(val: number){
+//         return val % 2 !== 0; // filter odd numbers in range of 1-5
+//     })
+//     .subscribe(x => console.log(x));
+//step09: reduce
+var Rx = require('rxjs/Rx');
+var source = Rx.Observable.range(1, 5)
+    .reduce(function (a, b) { return a + b; })
+    .subscribe(function (x) { return console.log(x); });
+//step10: reduce average
+// import Rx = require('rxjs/Rx');
+// var range = Rx.Observable.range(0, 5)
+//     .reduce((prev, cur) => {
+//         return {
+//             sum: prev.sum + cur,
+//             count: prev.count + 1
+//         };
+//     }, { sum: 0, count: 0 })
+//     .map((o) => {
+//         return o.sum / o.count;
+//     })
+//     .subscribe((x) => {
+//         console.log('Average is: ', x);
+//     }); 
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import { Observable } from 'rxjs/';
-import Rx from 'rxjs/Rx';
+// import { Observable } from 'rxjs/';
+// import Rx from 'rxjs/Rx';
 
+//step 02: filter
 // const source = Observable.fromEvent(document, 'click')
 //     .filter( function(x: MouseEvent) {
 //         return x.clientX > window.innerWidth / 2; // returns the clientX values greater than half of window's innerwidth
@@ -11,21 +12,72 @@ import Rx from 'rxjs/Rx';
 //             console.log('x: ' + x.clientX + ', y: ' + x.clientY);
 //     });
 
-const source : Rx.Observable<string> = Observable.create(
-    function(x: Rx.Observer<string>) {
-        x.next('Abc');
-        x.next('Xyz');
-        x.next('Pqr');
-        x.complete();
-});
+//step 03: create
+// const source : Rx.Observable<string> = Observable.create(
+//     function(x: Rx.Observer<string>) {
+//         x.next('Abc');
+//         x.next('Xyz');
+//         x.next('Pqr');
+//         x.complete();
+// });
 
-source.subscribe(
-    x => {
-        console.log('Next : ' +x);
-    },
-    err => {
-        console.log(err);
-    },
-    complete => {
-        console.log('Completed');
-    });
+// source.subscribe(
+//     x => {
+//         console.log('Next : ' +x);
+//     },
+//     err => {
+//         console.log(err);
+//     },
+//     function() {
+//         console.log('Completed');
+//     });
+
+//step 04 : ajax ni smjh aya??
+
+//step06: merge
+// import Rx = require('rxjs/Rx');
+
+
+// var a = Rx.Observable.interval(200).map(function(i: number) {
+//     return 'A' + i;
+// });
+// var b = Rx.Observable.interval(100).map(function(i: number) {
+//     return 'B' + i;
+// });
+
+// var m = Rx.Observable.merge(a, b)
+//     .take(10)
+//     .subscribe(x => { console.log(x);});
+
+//step08: filter even: filters an observable according to condition
+// import Rx = require('rxjs/Rx');
+
+// var src = Rx.Observable.range(1,5)
+//     .filter(function(val: number){
+//         return val % 2 !== 0; // filter odd numbers in range of 1-5
+//     })
+//     .subscribe(x => console.log(x));
+
+//step09: reduce
+import Rx = require('rxjs/Rx');
+
+const source = Rx.Observable.range(1,5)
+    .reduce((a: number, b: number) => {return a+b;})
+    .subscribe(x=> console.log(x));
+
+//step10: reduce average
+// import Rx = require('rxjs/Rx');
+
+// var range = Rx.Observable.range(0, 5)
+//     .reduce((prev, cur) => {
+//         return {
+//             sum: prev.sum + cur,
+//             count: prev.count + 1
+//         };
+//     }, { sum: 0, count: 0 })
+//     .map((o) => {
+//         return o.sum / o.count;
+//     })
+//     .subscribe((x) => {
+//         console.log('Average is: ', x);
+//     });
